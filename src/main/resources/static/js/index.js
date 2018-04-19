e0e6b4a76e17c00b0443c729cb8e57afb02a8e94
黄欣 <305617286@qq.com>
2018-04-19 17:35:56 +0800

    用户管理组件

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -18,13 +18,15 @@ var Login = {
             let psd = $("input[name='pwd']").val();
             let params = {
                 userName:user,
-                pwd:psd
+                password:psd
             }
             vm.getData(url,'POST',params,function(data){
                 console.log(data);
                 if(data.message == 'OK'){
                     console.log('登录成功');
-                    router.push({path:'/home'});
+                    router.push({path:'/home'}); //登录成功跳转至主页
+                    //设置用户信息的本地存储
+                    localStorage.setItem("userInfo",JSON.stringify(data.content));
                 }else{
                     console.log('用户不存在');
                 }
@@ -35,14 +37,42 @@ var Login = {
     },
 }
 var Home = {
-    template: '#home'
+    template: '#home',
+    data:function(){
+        let userInfo = JSON.parse(localStorage.getItem("userInfo")); //取出登录用户信息
+        if(userInfo == {} || userInfo == null){
+            console.log('not login');
+
+        }else{
+            console.log('user',userInfo);
+            return userInfo;
+        }
+    }
+}
+var userManage = {
+    template: '#userManage'
 }
 
 /* 定义路由 */
 var routes = [
-    {path:"/login",component:Login},
-    {path:"/home",component:Home},
-    {path:"/",redirect:'/login'}
+    {
+        path:"/login",
+        component:Login
+    },
+    {
+        path:"/home",
+        component:Home,
+        chidren:[
+            {
+                path:'userManage',
+                component:userManage
+            }
+        ]
+    },
+    {
+        path:"/",
+        redirect:'/login'
+    }
 ]
 
 /* 创建路由器 */
