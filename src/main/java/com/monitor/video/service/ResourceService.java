0691300b44b06e078659343c28daf75ba9b7dfc8
wdl5i <83813986@qq.com>
2018-04-27 23:00:46 +0800

    授权API

--- src/main/java/com/monitor/video/service/ResourceService.java
+++ src/main/java/com/monitor/video/service/ResourceService.java
@@ -20,11 +20,26 @@ public class ResourceService extends AbstractService<Resource> {
         super.setDao(dao);
     }
 
-    public RestResult<List<Resource>> list(String userId) {
+    public RestResult<List<Resource>> list(int userId) {
         RestResult<List<Resource>> restResult;
         try {
             List<Resource> resources =  dao.list(userId);
             restResult = RestResult.buildSuccessResult(resources);
+        } catch (Exception e) {
+            restResult = RestResult.buildErrorResult(RestResult.Status.INTERNAL_SERVER_ERROR);
+        }
+        return restResult;
+    }
+
+    public RestResult auth(int userId, int resourceId, boolean value) {
+        RestResult<List<Resource>> restResult;
+        try {
+            if(value) {
+                dao.addAuth(userId, resourceId);
+            } else {
+                dao.deleteAuth(userId, resourceId);
+            }
+            restResult = RestResult.buildSuccessResult();
         } catch (Exception e) {
             restResult = RestResult.buildErrorResult(RestResult.Status.INTERNAL_SERVER_ERROR);
         }
