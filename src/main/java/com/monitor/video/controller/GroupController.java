840db52e688049bf22d9f5b262513e4cd21f3d18
wdl5i <83813986@qq.com>
2018-05-02 10:52:20 +0800

    删除组与设备URL地址修改

--- src/main/java/com/monitor/video/controller/GroupController.java
+++ src/main/java/com/monitor/video/controller/GroupController.java
@@ -46,7 +46,7 @@ public class GroupController extends AbstractController<Group> {
         return service.addGroupFacility(groupId, facilityId);
     }
 
-    @DeleteMapping("/groupFacility/{userId}/{groupId}")
+    @DeleteMapping("/facility/{userId}/{groupId}")
     public RestResult removeGroupFacility(@PathVariable String groupId, @PathVariable String facilityId) {
         return service.removeGroupFacility(groupId, facilityId);
     }
